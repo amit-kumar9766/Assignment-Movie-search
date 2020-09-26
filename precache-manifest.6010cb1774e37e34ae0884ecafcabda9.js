e054f0c7b6d820dd4c6f08b39d0e9aedadb90e8e
amit-kumar9766 <amitbalharakr93@gmail.com>
2020-09-27 01:55:21 +0530

    Updates

---
+++ precache-manifest.6010cb1774e37e34ae0884ecafcabda9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd82f8831b3577ac52c568c693d927f5",
+    "url": "/Assignment-Movie-search/index.html"
+  },
+  {
+    "revision": "3a9b88dd620f4c1645cc",
+    "url": "/Assignment-Movie-search/static/css/2.a002812b.chunk.css"
+  },
+  {
+    "revision": "76eb966bde5ee03e004d",
+    "url": "/Assignment-Movie-search/static/css/main.55eb3045.chunk.css"
+  },
+  {
+    "revision": "3a9b88dd620f4c1645cc",
+    "url": "/Assignment-Movie-search/static/js/2.630364c2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Assignment-Movie-search/static/js/2.630364c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76eb966bde5ee03e004d",
+    "url": "/Assignment-Movie-search/static/js/main.0b390c87.chunk.js"
+  },
+  {
+    "revision": "4bfaba3b04c645ba7a28",
+    "url": "/Assignment-Movie-search/static/js/runtime-main.aa7a3248.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Assignment-Movie-search/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Assignment-Movie-search/static/media/arrow_right.8ef6a08c.svg"
+  }
+]);
